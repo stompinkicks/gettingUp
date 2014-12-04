db9f87d827c1fab7d7523991163159937bdb50f8
Alexander Auld <alexander.auld@hyperisland.se>
2014-12-04 21:21:12 +0900

    Added image validation and styles for uploading images

--- app/models/pattern.rb
+++ app/models/pattern.rb
@@ -2,12 +2,13 @@ class Pattern < ActiveRecord::Base
   attr_accessible :description, :image
 
   belongs_to :user
-  has_attached_file :image
+  has_attached_file :image, styles: { medium:"320x240" }
 
   validates :description, presence: true, :length => { :maximum => 150 }
   validates :user_id, presence: true
   validates_attachment :image, presence: true,
 		  							   content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']},
 		  							   size: { less_than: 5.megabytes }
+
 
 end

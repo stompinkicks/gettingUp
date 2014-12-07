70490daece511efefad4b92cae6664183cd396e2
Alexander Auld <alexander.auld@hyperisland.se>
2014-12-07 20:56:02 +0900

    Re-routed patterns to be the home page and added cases when a user is not signed in the see the sign up prompt

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ GettingUp::Application.routes.draw do
 
   get 'about' => 'pages#about'
 
-  root :to => 'pages#home'
+  root :to => 'patterns#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
